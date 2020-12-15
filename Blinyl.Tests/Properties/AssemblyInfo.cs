c951f38a1e0dacc816695e39359d2ee73ee11680
jcharfauros <julia.charfauros@gmail.com>
2020-12-14 23:10:54 -0500

    Add project files.

---
+++ Blinyl.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Blinyl.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Blinyl.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b72e2253-0c58-4fee-a22e-5a8aaa0a5478")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Blinyl.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Blinyl.Tests
+{
+    [TestClass]
+    public class UnitTests 
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
