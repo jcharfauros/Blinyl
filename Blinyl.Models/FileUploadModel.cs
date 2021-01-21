cfe6604d9ca9b03e45e9ff292c1fa65dd3bf9a38
jcharfauros <julia.charfauros@gmail.com>
2021-01-20 20:32:15 -0500

    uploading a file functionality added

---
+++ Blinyl.Models/FileUploadModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blinyl.Models
+{
+    public class FileUploadModel
+    {
+        [DataType(DataType.Upload)]
+        [Display(Name = "Upload File")]
+        [Required(ErrorMessage = "Please choose file to upload.")]
+        public string file { get; set; }
+    }
+}
