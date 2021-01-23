3e3bfd8c6db4da5df13551526da67372eb8a066a
julia charfauros <julia.charfauros@gmail.com>
2021-01-22 20:12:57 -0500

    Added to Wishlist data table

--- Blinyl.Data/Wishlist.cs
+++ Blinyl.Data/Wishlist.cs
@@ -11,17 +11,20 @@ namespace Blinyl.Data
     public class Wishlist
     {
         [Key] 
-        public int WishId { get; set; }
-        [ForeignKey("UserId")]        
-        public virtual string UserId { get; }
-        [ForeignKey("ToyId")]
-        public virtual int ToyId { get; }
-        public virtual ICollection<Toy> Toys { get; set; }
+        public int WishId { get; set; }                
+               
+        [ForeignKey(nameof(Toy))]
+        public int ToyId { get; set; }
+        public virtual Toy Toy { get; set; }
+        //public virtual ICollection<Toy> Toys { get; set; }
+        [ForeignKey(nameof(ApplicationUser))]
+        //public virtual string UserId { get; }
         [Required][MaxLength(45), MinLength(5)] 
+        [Display(Name="Wish List Name")]
         public string WishListTitle { get; set; }
-        [Display(Name ="Created")]
+        [Display(Name ="Created on")]
         public DateTimeOffset CreateUtc { get; set; }
-        [Display(Name ="Modified")]
+        [Display(Name ="Modified on")]
         public DateTimeOffset ModifiedUtc { get; set; }
     }
 }
